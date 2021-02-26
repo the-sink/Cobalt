eef163f2107b308de7a1372ee2472872572686c6
TheSink <lucianedean@gmail.com>
2021-02-26 15:52:19 -0800

    Edit config/package files, add inspirobot cmds

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,5 @@
+const fetch = require("node-fetch");
+
 module.exports = async client => {
   // Log that the bot is online.
   client.logger.log(`${client.user.tag}, ready to serve ${client.users.cache.size} users in ${client.guilds.cache.size} servers.`, "ready");
@@ -12,6 +14,16 @@ module.exports = async client => {
     client.application = await client.fetchApplication();
     client.application.team ? client.owners.push(...client.application.team.members.keys()) : client.owners.push(client.application.owner.id);
   }, 60000);
+
+  (async () => {
+    try {
+        await fetch('https://inspirobot.me/api?getSessionID=1')
+          .then(res => res.text())
+          .then(body => { client.settings.set('inspireSessionKey', body); });
+    } catch (err) {
+        client.logger.warn(`Unable to retrieve a new session key for the InspiroBot audio command: ${err}`)
+    };
+  })();
 
   // Make the bot "play the game" which is the help command with default prefix.
   client.user.setActivity(`${client.settings.get("default").prefix}help`, {type: "PLAYING"});
