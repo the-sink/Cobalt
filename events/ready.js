72b2ef59e8770f67e604ca7eab22d742cfb7f594
TheSink <lucianedean@gmail.com>
2021-02-28 23:00:49 -0800

    Remove more multi-server/config stuff

--- events/ready.js
+++ events/ready.js
@@ -40,5 +40,5 @@ module.exports = async client => {
   client.logger.log(`Loaded ${numModules} modules.`);
 
   // Make the bot "play the game" which is the help command with default prefix.
-  client.user.setActivity(`${client.settings.get("default").prefix}help | v${process.env.npm_package_version}`, {type: "PLAYING"});
+  client.user.setActivity(`${client.config.prefix}help | v${process.env.npm_package_version}`, {type: "PLAYING"});
 };
