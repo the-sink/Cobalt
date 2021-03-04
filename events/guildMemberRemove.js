70a2326e7b826ad0bec18888a765cd77c54b57ca
TheSink <lucianedean@gmail.com>
2021-03-03 19:38:01 -0800

    Add automatic demote (testing needed)

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,6 +1,18 @@
 // This event executes when a member leaves the guild.
+const fetch = require("node-fetch");
 
-module.exports = (client, member) => {
-    
+module.exports = async (client, member) => {
+    client.logger.log(`Guild member left: ${member.id}`);
+
+    if (client.settings.has(`uidCache_${member.id}`)) {
+        let cachedId = client.settings.get(`uidCache_${member.id}`);
+        let currentRole = await client.roblox.getRankInGroup(970502, cachedId);
+
+        if (currentRole == 2) {
+            await client.roblox.setRank({group: 970502, target: cachedId, rank: 1});
+            client.settings.delete(`uidCache_${member.id}`);
+            client.logger.log(`User ${cachedId} was demoted for leaving the server as a Project Tester.`);
+        }
+    }
 };
   
