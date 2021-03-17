1d061623f7acc3ce98fae96ceb6f53570420571d
TheSink <lucianedean@gmail.com>
2021-03-17 00:13:21 -0700

    Format change for mylevel command

--- commands/mylevel.js
+++ commands/mylevel.js
@@ -1,6 +1,6 @@
 exports.run = async (client, message, args, level) => {
   const friendly = client.config.permLevels.find(l => l.level === level).name;
-  message.reply(`Your permission level is: ${level} - ${friendly}`);
+  message.reply(`Your permission level is: \`${level} (${friendly})\``);
 };
 
 exports.conf = {
