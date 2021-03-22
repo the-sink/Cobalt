bf15d55f97785576c85b6a8b63e4c3adebfbcdbf
TheSink <lucianedean@gmail.com>
2021-03-21 23:41:44 -0700

    Add rate limit to ai commands

--- commands/horse.js
+++ commands/horse.js
@@ -1,11 +1,15 @@
 const fetch = require("node-fetch");
 const Discord = require("discord.js");
+let lastRun = Date.now();
 
 exports.run = async (client, message, args, level) => {
-  message.channel.startTyping();
-  const image = await fetch("https://thishorsedoesnotexist.com/");
-  message.channel.send({files: [new Discord.MessageAttachment(image.body)]});
-  message.channel.stopTyping();
+  if (Date.now()-lastRun > 3000) {
+    lastRun = Date.now();
+    message.channel.startTyping();
+    const image = await fetch("https://thishorsedoesnotexist.com/");
+    message.channel.send({files: [new Discord.MessageAttachment(image.body)]});
+    message.channel.stopTyping();
+  }
 };
 
 exports.conf = {
