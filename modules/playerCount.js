6489e3964571999bd7b7c6b9aef9b9e7b6d4e61c
TheSinkGod <lucianedean@gmail.com>
2021-06-30 00:12:18 -0700

    Use api instead of noblox for player count

--- modules/playerCount.js
+++ modules/playerCount.js
@@ -1,9 +1,6 @@
-module.exports = async (client) => {
-    if (client.roblox === undefined) {
-        client.logger.warn("Skipping startup of activity module because no Roblox cookie is defined.");
-        return;
-    }
+const fetch = require("node-fetch");
 
+module.exports = async (client) => {
     var channel;
 
     if (client.config.channels.serverList !== undefined && client.config.modules.serverList) {
@@ -16,11 +13,15 @@ module.exports = async (client) => {
 
     async function main() {
         while (true) {
-            var info = await client.roblox.getPlaceInfo(257223268);
+            var info;
+            await fetch('https://games.roblox.com/v1/games?universeIds=105407262')
+                .then(res => res.json())
+                .then(body => {info = body.data[0]});
+            console.log(info);
             if (channel !== undefined){
-                channel.setTopic(`${info.OnlineCount} online players | ${numberWithCommas(info.VisitedCount)} total visits`)
+                channel.setTopic(`${info.playing} online players | ${numberWithCommas(info.visits)} total visits`)
             }
-            client.user.setActivity(`;help | ${info.OnlineCount} playing CVRF`, { type: 'PLAYING' })
+            client.user.setActivity(`;help | ${info.playing} playing CVRF`, { type: 'PLAYING' })
                 .catch(console.error);
             await new Promise(resolve => setTimeout(resolve, 60000)); // Updates every 60 seconds
         }
