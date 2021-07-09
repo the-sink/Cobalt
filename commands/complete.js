52c6adf2198328c4a7cf5494196d540b6e30f470
TheSink <lucianedean@gmail.com>
2021-07-09 00:04:43 -0700

    why

--- commands/complete.js
+++ commands/complete.js
@@ -23,7 +23,11 @@ exports.run = async (client, message, args, level) => {
                             var text = await res.text();
                             var response = str + text.replace("<|endoftext|>", " "); // `<@${message.author.id}>, ` + 
                             msg.delete();
-                            message.reply(response.substring(0, 1990));
+                            message.reply(response.substring(0, 1900)).catch(err => {
+                                running = false;
+                                msg.edit(`<@${message.author.id}>, an error has occurred posting the response to Discord.`);
+                                ///////////// TODO: Supply a text file instead
+                            });
                         } else {
                             running = false;
                             msg.edit(`<@${message.author.id}>, an error has occurred generating the rest of that prompt.`);
