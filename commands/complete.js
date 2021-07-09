19df87326e98e8a45ced1c56e94ca927e4ba10d9
TheSink <lucianedean@gmail.com>
2021-07-08 23:21:11 -0700

    clean up ;complete command

--- commands/complete.js
+++ commands/complete.js
@@ -1,37 +1,33 @@
 const fetch = require("node-fetch");
 const fs = require("fs");
+var running = false;
 
 exports.run = async (client, message, args, level) => {
     var str = message.content.replace(`${client.config.prefix}complete `, "");
     message.reply("<a:loading:776537774391164949> Completing your prompt (this will probably take around 2 minutes)...")
         .then(msg => {
-            fetch('http://192.168.1.92:7001', {
-                    method: 'post',
-                    body:    str,
-                    headers: { 'Content-Type': 'text/plain' },
-                })
-                .then(res => {
-                    if (res.ok) {
-                        return res;
-                    } else {
-                        if (res.status == 409) {
-                            msg.edit(`<@${msg.author.id}>, the bot is already running a completion task! Please wait for it to finish first.`);
-                        } else {
-                            msg.edit(`<@${msg.author.id}>, an error occured while executing the completion task. If this keeps happening, something is *probably* wrong.`);
-                        }
-                        return;
-                    }
-                })
-                .then(res => res.text())
-                .then(async function(text){
-                    fs.writeFileSync("completion.txt", str + text);
-                    msg.delete();
-                    message.channel.send(`<@${msg.author.id}>`, {
-                        files: [
-                            "completion.txt"
-                        ]
+            try {
+                fetch('http://192.168.1.92:7001', {
+                        method: 'post',
+                        body:    str,
+                        headers: { 'Content-Type': 'text/plain' },
                     })
-                });
+                    .then(res => {
+                        if (res.ok) {
+                            var text = res.text();
+                            var response = `<@${message.author.id}>, ` + str + text;
+                            msg.edit(response.substring(0, 1999));
+                        } else if (res.status == 409) {
+                            running = false;
+                            msg.edit(`<@${message.author.id}>, the bot is already running a completion task! Please wait for it to finish first.`);
+                        } else {
+                            throw 'Server error';
+                        }
+                    });
+            } catch(err){
+                running = false;
+                msg.edit(`<@${message.author.id}>, an error occured while executing the completion task. If this keeps happening, something is *probably* wrong.`);
+            }
         });
 };
 
