ab9289912ca7b0305de6ab2e514d7f68c2ba81ae
TheSink <lucianedean@gmail.com>
2021-12-22 15:54:40 -0800

    delete watchdog timer entry on stop

--- modules/serverList.js
+++ modules/serverList.js
@@ -96,6 +96,7 @@ let actions = {
         // Remove server message and clear from list
         servers[json.serverKey].message.delete();
         delete servers[json.serverKey];
+        delete watchdogList[key];
         sendResponse(res, 200, "Success");
     }
 }
