e7de48560aca744ab5e8a3d738f23264e5cabb01
TheSink <lucianedean@gmail.com>
2021-12-26 22:05:42 -0800

    add secret feature(tm)

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,10 +1,21 @@
+const fetch = require("node-fetch");
+
 module.exports = async (client, message) => { // https://labs.bible.org/api/?passage=random&type=json
   if (message.author.bot) return;
   if (client.inspectRestricted){
     client.inspectRestricted(message)
   }
 
-  if (true) { // Math.floor(Math.random() * 100) == 1
-    message.reply("test");
+  if (Math.floor(Math.random() * 100) == 1) {
+    await fetch("https://labs.bible.org/api/?passage=random&type=json")
+    .then(res => res.json())
+    .then(body => {
+      message.reply(body[0]['text'])
+      .then(message => {
+        setTimeout(function(){
+          message.delete();
+        }, 2500);
+      });
+    });
   }
 };
